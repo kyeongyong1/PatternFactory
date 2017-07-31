1a23809b009b43cb6d993be1f2a4bd820bacb02e
Lee <kyeongyong1@github.com>
2017-07-31 23:35:01 +0900

    Initial Commit

---
+++ PatternFactory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PatternFactory
+//
+//  Created by Lee on 2017. 7. 31..
+//  Copyright © 2017년 LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
