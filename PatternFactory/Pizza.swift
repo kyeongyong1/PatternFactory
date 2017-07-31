e03b2f45c60ddb79b7c53be024ed5e09324b945d
Lee <kyeongyong1@github.com>
2017-08-01 00:47:34 +0900

    simple factory

---
+++ PatternFactory/Pizza.swift
@@ -0,0 +1,9 @@
+//
+//  Pizza.swift
+//  PatternFactory
+//
+//  Created by Lee on 2017. 7. 31..
+//  Copyright © 2017년 LEE. All rights reserved.
+//
+
+import Foundation
---
+++ PatternFactory/PizzaStore.swift
@@ -0,0 +1,9 @@
+//
+//  PizzaStore.swift
+//  PatternFactory
+//
+//  Created by Lee on 2017. 7. 31..
+//  Copyright © 2017년 LEE. All rights reserved.
+//
+
+import Foundation
